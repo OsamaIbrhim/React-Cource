2597cc57c05396159b786901567048b2a65c4ee8
Osama Ibrahim <osamaibrhiim@gmail.com>
2022-05-01 18:21:42 +0200

    1 May 22

---
+++ indecision-app/public/scripts/app.js
@@ -0,0 +1,6 @@
+"use strict";
+
+console.log('console.log : is not working');
+var templet = /*#__PURE__*/React.createElement("p", null, "Hiiiiiiii");
+var add = document.getElementById('add');
+ReactDOM.render(templet, add);
---
+++ indecision-app/src/app.js
@@ -0,0 +1,7 @@
+console.log('console.log : is not working')
+
+const templet = <p>Hiiiiiiii</p>;
+
+const add = document.getElementById('add')
+
+ReactDOM.render(templet,add)
